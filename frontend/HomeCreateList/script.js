e5dcc5710a2cfbd64a499f0fcc1b47e1e2d5af2c
PedroHenriqweM <104871675+PedroHenriqweM@users.noreply.github.com>
2025-03-03 02:33:44 -0300

    Adição das Funções Auxiliares

---
+++ frontend/HomeCreateList/script.js
@@ -0,0 +1,29 @@
+// ==================== FUNÇÕES AUXILIARES ====================
+function goBack() {
+  window.history.back();
+}
+
+function goToLister() {
+  window.location.href = "profile43.html";
+}
+
+function goToRegisterStarTup() {
+  window.location.href = "profile41.html";
+}
+
+function goToStart() {
+  window.location.href = "profileInicio.html";
+}
+
+function goToMemberManagement() {
+  window.location.href = "caminho/para/MemberManagement";
+}
+
+function goToStartupManage() {
+  window.location.href = "caminho/para/StartupManage";
+}
+
+function goToCreation() {
+  window.location.href = "profile41.html";
+}
+
